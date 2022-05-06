278320e2db569f5919f31913478d78c331c09e80
ccrsxx <aminrisal@gmail.com>
2022-05-06 21:16:56 +0700

    fixed header width

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -10,7 +10,7 @@ export function Container({ onSubmit, children }: ContainerProps) {
   const { handleSubmit } = useFormContext();
 
   return (
-    <main className='w-[90vw] max-w-md rounded-lg bg-white px-8 py-9 text-black sm:w-full'>
+    <main className='w-[90vw] rounded-lg bg-white px-8 py-9 text-black sm:max-w-md'>
       <form
         className='flex flex-col gap-4 children:relative'
         onSubmit={handleSubmit(onSubmit)}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <header className='mt-2 p-2 text-center sm:mt-4'>
+    <header className='mt-2 w-[90vw] max-w-md py-2 text-center sm:mt-4'>
       <h1 className='text-4xl font-bold'>Simple Form</h1>
       <h2 className='mt-2 text-xl font-light italic opacity-80'>
         Thank you for taking the time to complete my form
