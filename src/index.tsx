830c4ecdea5bb5f0fc89e33dd04c23dd506b6100
ccrsxx <aminrisal@gmail.com>
2022-05-15 11:47:15 +0700

    use sass instead off css

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
-import './index.css';
+import './index.scss';
 
 const container = document.getElementById('root') as HTMLDivElement;
 const root = createRoot(container);
